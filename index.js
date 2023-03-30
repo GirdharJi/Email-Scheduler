ebfb2027ba5e013993e7c545d4059acb114cbe6c
GirdharJi <girdharkartik@gmail.com>
2023-03-30 17:14:13 +0530

    scheduler function added using node-cron

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const scheduler = require("node-cron");
 const app = express();
 
 const PORT = 1338;
+
+scheduler.schedule("* * * * * *", async() => {
+    console.log("I am running");
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
